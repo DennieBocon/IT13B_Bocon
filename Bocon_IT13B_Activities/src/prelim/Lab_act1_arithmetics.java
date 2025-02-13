d4fb2c79d955983929745fabe591dc87863dc513
denniesbocon <bocondenniesburt@gmail.com>
2025-02-13 12:29:36 +0800

    Added Hello World

---
+++ Bocon_IT13B_Activities/src/prelim/Lab_act1_arithmetics.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prelim;
+
+/**
+ *
+ * @author User
+ */
+public class Lab_act1_arithmetics {
+    
+    int y = 10;
+        int x = 4;
+        int n = 6;
+
+       
+        System.out.println(y + " * " + x + " + " + 6 + " = " + (y * x + n));
+        
+    
+}
